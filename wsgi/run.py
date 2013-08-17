522d752cd143d0b0905ce8e66dccfc41c7d7ae0a
Miguel Provencio <miguel.a.provencio@gmail.com>
2013-08-17 08:18:34 -0500

    use module instead of package

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,3 +1,15 @@
-from app import app
+from flask import Flask,render_template
+ 
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/oneAndHalfMileRunWalk')
+def oneAndHalfMileRunWalk():
+    return render_template("oneAndHalfMileRunWalk.html")
+
 if __name__ == "__main__":
     app.run(debug = True) #We will set debug false in production 
