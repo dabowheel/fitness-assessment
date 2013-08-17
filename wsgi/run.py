4c49f9eaf638bb7f85ac5a06f0ded7bdebc03c01
Miguel Provencio <miguel.a.provencio@gmail.com>
2013-08-17 08:29:58 -0500

    debug root path

--- wsgi/run.py
+++ wsgi/run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return "root path is " + app.root_path
 
 @app.route('/oneAndHalfMileRunWalk')
 def oneAndHalfMileRunWalk():
